2a4d7d6b782cd67060a4771ec1e27fca21ec0efb
idil <idil.iman1999@gmail.com>
2023-08-27 15:10:48 +0800

    chore: implement ssr

--- src/app/_components/TodoList.tsx
+++ src/app/_components/TodoList.tsx
@@ -2,9 +2,18 @@
 
 import { useState } from 'react';
 import { trpc } from '../_trpc/client';
+import { serverClient } from '../_trpc/serverClient';
 
-export default function TodoList() {
-  const getTodos = trpc.getTodos.useQuery();
+export default function TodoList({
+  initialTodos,
+}: {
+  initialTodos: Awaited<ReturnType<(typeof serverClient)['getTodos']>>;
+}) {
+  const getTodos = trpc.getTodos.useQuery(undefined, {
+    initialData: initialTodos,
+    refetchOnMount: false,
+    refetchOnReconnect: false,
+  });
   const addTodo = trpc.addTodo.useMutation({
     onSettled: () => {
       getTodos.refetch();
@@ -19,7 +28,27 @@ export default function TodoList() {
   const [content, setContent] = useState<string>('');
 
   return (
-    <div>
+    <div className='flex flex-col space-y-5 items-center'>
+      <div className='flex space-x-3'>
+        <label htmlFor='content'>Content</label>
+        <input
+          className='text-black border bg-slate-100'
+          id='content'
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
+        <button
+          onClick={async () => {
+            if (content.length) {
+              addTodo.mutate(content);
+              setContent('');
+            }
+          }}
+        >
+          Add Todo
+        </button>
+      </div>
+
       <div>
         {getTodos?.data?.map((todo) => (
           <div key={todo.id}>
@@ -34,24 +63,9 @@ export default function TodoList() {
                 });
               }}
             />
-
             <label htmlFor={`check-${todo.id}`}>{todo.content}</label>
           </div>
         ))}
-      </div>
-      <div>
-        <label htmlFor='content'>Content</label>
-        <input className='text-black' id='content' value={content} onChange={(e) => setContent(e.target.value)} />
-        <button
-          onClick={async () => {
-            if (content.length) {
-              addTodo.mutate(content);
-              setContent('');
-            }
-          }}
-        >
-          Add Todo
-        </button>
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image';
 import TodoList from './_components/TodoList';
+import { serverClient } from './_trpc/serverClient';
 
-export default function Home() {
+export default async function Home() {
+  const todos = await serverClient.getTodos();
+
   return (
     <main className='max-w-3xl mx-auto mt-5'>
-      <TodoList />
+      <TodoList initialTodos={todos} />
     </main>
   );
 }
